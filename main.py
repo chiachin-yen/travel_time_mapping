1d4b324989859aeaad71de06bef3739ae9f0b60f
chiachin-yen <jason.melon@gmail.com>
2018-09-21 13:44:01 +0800

    add ignored path to store temp files, read config in main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+"""
+Main script of traveling time
+"""
+
+import configparser
+import json
+import os
 import urllib
+
+config = configparser.ConfigParser()
+if os.path.isfile('setting.ini'):
+    config.read('setting.ini')
+else:
+    config['API'] = {
+        'KEY': input("Paste your API here: ")
+    }
+    print("API Key Saved.")
+
+    with open('setting.ini', 'w') as configfile:
+        config.write(configfile)
+
+    config.read('setting.ini')
+
+print(config['API']['KEY'])
