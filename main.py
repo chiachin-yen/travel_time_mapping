50aa3a1292b314a97132f4a983f6999ed244c4e5
chiachin-yen <jason.melon@gmail.com>
2018-09-25 08:13:03 +0800

    Add gen_grid()

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import json
 import os
 import urllib
 
+# Load API KEY
 config = configparser.ConfigParser()
 if os.path.isfile('setting.ini'):
     config.read('setting.ini')
@@ -21,4 +22,15 @@ else:
 
     config.read('setting.ini')
 
-print(config['API']['KEY'])
+
+def gen_grid(cen_X, cen_Y, step, count_X, count_Y):
+    '''Generate a matrix of coordinates'''
+    x = y = 0
+    dx = 0
+    dy = -1
+    for i in range(max(count_X, count_Y)**2):
+        if (-count_X/2 < x <= count_X/2) and (-count_Y/2 < y <= count_Y/2):
+            yield [cen_X + x * step, cen_Y + y * step]
+        if x == y or (x < 0 and x == -y) or (x > 0 and x == 1-y):
+            dx, dy = -dy, dx
+        x, y = x + dx, y + dy
